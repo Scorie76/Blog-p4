ee16f0325076d47456db5d9de250781d14f7cf5c
unknown <guillaume.morillon@gmx.fr>
2019-03-15 13:39:11 +0100

    ajout des fichiers header et footer

---
+++ footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Portfolio Guillaume Morillon</title>
+  <meta name="description" content="Portfolio Guillaume Morillon">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+
+
+</head>
+<body data-spy="scroll" data-target=".navbar">
+  
+
+  <div class="container-fluid">
+    <div class="row">
+    <footer class="col-xs-12">
+      <div id="reseau1" class="col-xs-offset-9 col-xs-1">
+        <a href="">
+          <i class="fab fa-twitter-square fa-2x"></i>
+        </a>
+      </div>
+      <div  id="reseau2" class="col-xs-1">
+        <a href="">
+          <i class="fab fa-facebook fa-2x"></i>
+        </a>
+      </div> 
+      <div id="reseau3" class="col-xs-1">
+        <a href="">
+          <i class="fas fa-rss fa-2x"></i>
+        </a>
+      </div>
+    </footer>
+  </div>
+  </div>
+  <!-- jQuery -->
+  <script src="js/jquery-3.3.1.js"></script>
+  <!-- Javascript de Bootstrap -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
