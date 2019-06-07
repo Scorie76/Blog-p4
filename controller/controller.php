a3b94c81f24d13eecec0b3ddc38c8599134efad2
unknown <guillaume.morillon@gmx.fr>
2019-06-07 13:34:06 +0200

    ajout dossier backend

--- controller/controller.php
+++ controller/controller.php
@@ -1,36 +1,72 @@
 <?php
 
-require('model/model.php');
+// Chargement des classes
+
+require_once('model/model.php');
 
 function listPosts()
 {
-    $req = getPosts();
+    $frontManager = new FrontManager(); // Création d'un objet
+    $req = $frontManager -> getPosts(); // Appel d'une fonction de cet objet
 
     require('view/billet.php');
 }
 
+
+
 function post()
+
 {
-    $post = getPost($_GET['billet']);
-    $comments = getComments($_GET['billet']);
+
+    $frontManager = new FrontManager(); // Création d'un objet
+
+
+    $post = $frontManager->getPost($_GET['billet']);
+    $comments = $frontManager->getComments($_GET['billet']);
 
     require('view/billet_lecture.php');
 }
 
 function addComment($postId, $author, $comment)
 {
-    $affectedLines = postComment($postId, $author, $comment);
+    $frontManager = new FrontManager(); // Création d'un objet
+
+    $affectedLines = $frontManager->postComment($postId, $author, $comment);
 
     if ($affectedLines === false) {
         die('Impossible d\'ajouter le commentaire !');
     }
     else {
-        header('Location: index.php?action=post&billet=' . $postId);
+      //header('Location: index.php?action=post&billet=' . $postId);
+     
+        ?>
+    <script language="javascript">window.history.back(); </script>;<?php
+    }
+    
+}
+
+function connection($login, $pass)
+{
+
+    $frontManager = new FrontManager(); // Création d'un objet
+
+    $affectedLines = $frontManager->loginAdmin($login, $pass);
+    ?>
+    <script language="javascript">document.location="index.php"</script>;<?php
+}
+
+function mailing($nom, $prenom,$textarea, $adresseMail)
+{
+    $frontManager = new FrontManager(); // Création d'un objet
+
+    $affectedLines = $frontManager->envoiMailing($nom, $prenom,$textarea, $adresseMail);
+   
+    if ($affectedLines === false) {
+        die('Impossible d\'envoyer le message !');
+    }
+    else {
+        ?>
+    <script language="javascript">document.location="index.php"</script>;<?php
     }
 }
 
-function connection()
-{
-    $req = loginAdmin();
-    require ('view/login.php');
-}
--- controller/controllerFrontEnd.php
+++ controller/controllerFrontEnd.php
@@ -1,25 +1,41 @@
 <?php
 
-require('../model/modelFrontEnd.php');
+// Chargement des classes
+
+require_once('../model/modelFrontEnd.php');
 
 function listPosts()
 {
-    $req = getPosts();
+    $backManager = new BackManager(); // Création d'un objet
+    $req = $backManager -> getPosts(); // Appel d'une fonction de cet obj
 
     require('../view/redactor.php');
 }
 
 function post()
 {
-    $post = getPost($_GET['billet']);
-    $comments = getComments($_GET['billet']);
+    $backManager = new BackManager(); // Création d'un objet
+    $post = $backManager ->getPost($_GET['billet']);
+    $comments = $backManager ->getComments($_GET['billet']);
 
-    require('view/billet_lecture.php');
+    require('../view/redactor_lecture.php');
 }
+
+function mPost()
+{
+    $backManager = new BackManager(); // Création d'un objet
+    $post = $backManager ->getPost($_GET['billet']);
+    $comments = $backManager ->getComments($_GET['billet']);
+
+    require('../view/redactor_modification.php');
+}
+
+
 
 function addComment($postId, $author, $comment)
 {
-    $affectedLines = postComment($postId, $author, $comment);
+     $backManager = new BackManager(); // Création d'un objet
+    $affectedLines = $backManager ->postComment($postId, $author, $comment);
 
     if ($affectedLines === false) {
         die('Impossible d\'ajouter le commentaire !');
@@ -32,12 +48,142 @@ function addComment($postId, $author, $comment)
 function addPost($Title, $Content, $date_publi, $id_author)
 
 {
-    $affectedLines = postPost($Title, $Content, $date_publi, $id_author);
+    $backManager = new BackManager(); // Création d'un objet
+    $affectedLines = $backManager ->postPost($Title, $Content, $date_publi, $id_author);
 
     if ($affectedLines === false) {
         die('Impossible d\'ajouter le billet !');
     }
     else {
-        header('Location: indexFrontEnd.php');
+        //header('Location: indexFrontEnd.php');
+        ?>
+    <script language="javascript">document.location="indexFrontEnd.php"</script>;<?php
     }
+}
+
+
+function UDPost($Title, $Content, $date_publi, $id_author,$id)
+{
+    $backManager = new BackManager(); // Création d'un objet
+    $affectedLines = $backManager ->updatePost($Title, $Content, $date_publi, $id_author, $id);
+
+    if ($affectedLines === false) {
+        die('Impossible de modifier le billet !');
+         
+    }
+    else {
+        //header('Location: indexFrontEnd.php' );
+        ?>
+    <script language="javascript">document.location="indexFrontEnd.php"</script>;<?php
+    }
+}
+
+function dpost()
+{
+    $backManager = new BackManager();
+    $post = $backManager ->deletePost($_GET['billet']);
+   
+
+    //header('Location: indexFrontEnd.php' );
+    ?>
+    <script language="javascript">document.location="indexFrontEnd.php"</script>;<?php
+
+}
+
+function dComment()
+{
+    $backManager = new BackManager();
+    $post = $backManager ->deleteComment($_GET['id']);
+   
+
+    //header('Location: indexFrontEnd.php' );
+    ?>
+    <script language="javascript">document.location="indexFrontEnd.php"</script>;<?php
+
+}
+
+function avComment()
+{
+    $backManager = new BackManager();
+    $req = $backManager ->getavComments();
+
+    require('../view/redactor_AVComments.php');
+}
+
+function dBufferComment()
+{
+    $backManager = new BackManager();
+    $post = $backManager ->deleteBufferComment($_GET['id']);
+
+   //header('Location: indexFrontEnd.php?action=avComment' );
+    ?>
+    <script language="javascript">document.location="indexFrontEnd.php?action=avComment"</script>;<?php
+}
+
+function vBufferComment()
+{
+    $backManager = new BackManager();
+   
+    $post = $backManager ->validateBufferComment($_GET['id']);
+            $backManager ->deleteBufferComment($_GET['id']);
+    //header('Location: indexFrontEnd.php?action=avComment' );
+
+    ?>
+    <script language="javascript">document.location="indexFrontEnd.php?action=avComment"</script>;<?php
+
+
+
+    
+}
+
+function mBufferComment()
+{
+    $backManager = new BackManager();
+    $backManager ->MessageValidateBufferComment();
+   
+}
+
+
+
+function signalerComment()
+{
+    $backManager = new BackManager();
+    $post = $backManager ->validateSignalComment($_GET['id']);
+    header("Location: ../index.php?action=post&billet= 10 " );
+   
+
+   
+}
+
+function DisplaySComment()
+{
+    
+    $backManager = new BackManager();
+    $req = $backManager ->DisplayReportsComment();
+   
+
+   require('../view/redactor_signalComments.php');
+
+}
+
+function dSignalComment()
+{
+    $backManager = new BackManager();
+    $post = $backManager ->deleteSignalComment($_GET['id']);
+   
+
+   //header('Location: indexFrontEnd.php?action=DisplaySComment' );
+    ?>
+    <script language="javascript">document.location="indexFrontEnd.php?action=DisplaySComment"</script>;<?php
+}
+
+function cSignalComment()
+{
+    $backManager = new BackManager();
+    $post =  $backManager ->cancelSignalComment($_GET['id']);
+   
+
+   //header('Location: indexFrontEnd.php?action=DisplaySComment' );
+     ?>
+    <script language="javascript">document.location="indexFrontEnd.php?action=DisplaySComment"</script>;<?php
 }
